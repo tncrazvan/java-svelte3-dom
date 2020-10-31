bb1612433969d50ebcb62376d3433e36bbd5d724
Razvan <tangent.jotey@gmail.com>
2020-11-01 00:56:53 +0100

    first commit

---
+++ src/main/java/com/mycompany/svelte3dom/FileReaderJS.java
@@ -0,0 +1,27 @@
+package com.mycompany.svelte3dom;
+
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.graalvm.polyglot.Value;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class FileReaderJS {
+    public static String readString(Value filename) throws IOException{
+        return readString(filename, Value.asValue("UTF-8"));
+    }
+    public static String readString(Value filename, Value charset) throws IOException{
+        return Files.readString(Path.of(filename.asString()), Charset.forName(charset.asString()));
+    }
+}
