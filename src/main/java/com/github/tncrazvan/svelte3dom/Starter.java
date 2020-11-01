0fbc071650cb475aa1dae297aebccdf2db055046
Razvan <tangent.jotey@gmail.com>
2020-11-01 04:16:36 +0100

    update

--- src/main/java/com/github/tncrazvan/svelte3dom/Starter.java
+++ src/main/java/com/github/tncrazvan/svelte3dom/Starter.java
@@ -5,15 +5,12 @@
  */
 package com.github.tncrazvan.svelte3dom;
 
-import java.nio.file.Path;
-
 /**
  *
  * @author Administrator
  */
 public class Starter {
     public static void main(String[] args) {
-        Svelte3DOM dom = new Svelte3DOM(Path.of(System.getProperty("user.dir")));
-        System.out.println(dom.compile("<p>hello world</p>"));
+    
     }
 }
--- src/main/java/com/github/tncrazvan/svelte3dom/Svelte3DOM.java
+++ src/main/java/com/github/tncrazvan/svelte3dom/Svelte3DOM.java
@@ -10,9 +10,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.proxy.ProxyObject;
 
 /**
  *
@@ -20,29 +23,35 @@ import org.graalvm.polyglot.proxy.ProxyObject;
  */
 public class Svelte3DOM {
     protected Context context;
+    private String internal;
     
-    public Svelte3DOM(Path wk){
-        context = Context
+    public Svelte3DOM(Path wd){
+        try {
+            context = Context
                     .newBuilder()
-                    .currentWorkingDirectory(wk)
+                    .currentWorkingDirectory(wd)
                     .allowAllAccess(true)
                     .allowIO(true)
                     .build();
-        
-        String tools = 
-        "const System = Java.type('java.lang.System');" +
-        "const FileReaderJS = Java.type('com.github.tncrazvan.svelte3dom.FileReaderJS');" +
-        "load('./node_modules/jvm-npm/src/main/javascript/jvm-npm.js');" +
-        "const { compile } = require('./compiler.js');" +
-        "const { component } = require('./compile.js');" 
-        ;
-        
-        context.eval("js",tools);
-        
-        Value ssr = context.eval("js", "(function(service){dom=service;});");
-        ssr.executeVoid(this);
-        
-        context.eval("js","console.log('JavaScript initialized.');");
+            
+            String tools =
+                    "const System = Java.type('java.lang.System');" +
+                    "const FileReaderJS = Java.type('com.github.tncrazvan.svelte3dom.FileReaderJS');" +
+                    "load('./node_modules/jvm-npm/src/main/javascript/jvm-npm.js');" +
+                    "const { compile } = require('./compiler.js');" +
+                    "const { component } = require('./compile.js');"
+                    ;
+            
+            context.eval("js",tools);
+            
+            Value ssr = context.eval("js", "(function(service){dom=service;});");
+            ssr.executeVoid(this);
+            
+            context.eval("js","console.log('JavaScript initialized.');");
+            internal = resolveInternal();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
     
     public Context getContext(){
@@ -59,15 +68,52 @@ public class Svelte3DOM {
         return result;
     }
     
-    public Value render(String compiledSource){
-        return render(compiledSource, new HashMap<>());
+    private final ConcurrentHashMap<String,String> bundles = new ConcurrentHashMap<>();
+    
+    public String getInternal(){
+        return internal;
+    }
+    private String resolveInternal() throws IOException{
+        return Files
+                .readString(Path.of("./node_modules/svelte/internal/index.js"))
+                .replace("'use strict';", "")
+                .replace("\"use strict\";", "")
+                .replace("exports\\..*", "")
+                .trim()
+                ;
     }
     
-    public Value render(String compiledSource, HashMap<String,Object> props){
-        compiledSource = compiledSource.replace("\"use strict\";","\"use strict\";(function (){\nconst exports = {};\n")+"\nreturn exports;})";
-        Value app = context.eval("js", compiledSource);
-        Value result = app.execute();
-        Value def = result.getMember("default");
-        return def.getMember("render").execute(ProxyObject.fromMap(props));
+    public String getBundle(String id){
+        return bundles.get(id);
     }
+    
+    public void bundle(String id, String compiledSource){
+        bundle(id, compiledSource, new HashMap<>());
+    }
+    
+    private static final Pattern internalPattern = Pattern.compile(".*require\\s*\\(\\s*\"svelte\\/internal\"\\s*\\)\\s*;");
+    
+    public void bundle(String id, String compiledSource, HashMap<String,Object> props){
+        String propsString = context.eval("js","(function (props){return JSON.stringify(props)})").execute(props).asString();
+        String[] pieces = internalPattern.split(compiledSource, 2);
+        compiledSource = pieces[0] + internal + pieces[1];
+        compiledSource = compiledSource
+                .replace("'use strict';", "var app = (function () {let exports={};")
+                .replace("\"use strict\";", "var app = (function () {let exports={};")
+                .replace(
+                        "exports.default = Component;",
+                        
+                            "var app = new Component({"
+                                + "target: document.body,"
+                                + "props: "+propsString
+                            + "});\n" +
+                            "return app;\n" +
+                        "}());"
+                )
+                ;
+        //System.out.println(compiledSource);
+        bundles.put(id, compiledSource);
+    }
+    
+    
 }
